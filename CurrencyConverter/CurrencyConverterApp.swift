e2910014183e5990ab6ce9ebd5ee49fefc1fdcdd
Jayden Kim <hkim@lcs.on.ca>
2025-02-27 09:35:19 -0500

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by kim heejun on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
